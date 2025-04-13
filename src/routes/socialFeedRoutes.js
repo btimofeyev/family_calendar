805dfe2106faf254bcef40398cd2efa6cfa1383c
ben timofeyev <btimofeyev@gmail.com>
2025-04-13 19:51:05 -0400

    multiple posts

--- src/routes/socialFeedRoutes.js
+++ src/routes/socialFeedRoutes.js
@@ -6,8 +6,9 @@ const { upload } = require('../middleware/imageUpload');
 
 router.use(authMiddleware);
 
+// Updated route to use upload.array instead of upload.single for multiple files
 router.get('/family/:familyId/posts', socialFeedController.getPosts);
-router.post('/family/:familyId/posts', upload.single('media'), socialFeedController.createPost);
+router.post('/family/:familyId/posts', upload.array('media', 4), socialFeedController.createPost);
 
 router.post('/posts/:postId/like', socialFeedController.toggleLike);
 router.post('/posts/:postId/comment', socialFeedController.addComment);
